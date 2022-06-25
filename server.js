1a95fe3f69ed6079aef340890b48ff71c54391af
vaibhavghaytadkar <vghaytadkar@gmail.com>
2022-06-25 19:51:05 +0530

    added cart page ,login and register pages

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const path= require('path');
 const expressLayout = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3000 
 
-app.get('/',function(req,res){
-    res.render('home');
-})
+
 //Assets
 app.use(express.static('public'));
 //set template engine
@@ -16,6 +14,19 @@ app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
 
+//Routes
+app.get('/',function(req,res){
+    res.render('home');
+})
+app.get('/cart',function(req,res){
+    res.render('customers/cart');
+})
+app.get('/login',function(req,res){
+    res.render('auth/login');
+})
+app.get('/register',function(req,res){
+    res.render('auth/register');
+})
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`);
 })
